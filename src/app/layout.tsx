fb43dda87591ad175790ad7bbe59642886119a94
marvelarian <deathailc@gmail.com>
2025-12-30 15:41:57 +0530

    aws2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
 import { GlobalFetchLoader } from '@/components/system/GlobalFetchLoader'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Grid Trading Bot',
@@ -17,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <GlobalFetchLoader />
         {children}
       </body>
